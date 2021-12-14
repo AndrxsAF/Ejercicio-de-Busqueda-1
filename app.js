cf74829beb216f82718aa000d27aa76465037064
AndrxsAF <andrxsaf@gmail.com>
2021-12-14 19:44:07 +0000

    ejercicio resuelto

---
+++ app.js
@@ -0,0 +1,32 @@
+const listNumber = [
+    1,  2,  3,   4,  5,  6,  7,  8,  9, 10, 11, 12,
+   13, 14, 15,  16, 17, 18, 19, 20, 21, 22, 23, 24,
+   25, 26, 27,  28, 29, 30, 31, 32, 33, 34, 35, 36,
+   37, 38, 39,  40, 41, 42, 43, 44, 45, 46, 47, 48,
+   49, 50, 51,  52, 53, 54, 55, 56, 57, 58, 59, 60,
+   61, 62, 63,  64, 65, 66, 67, 68, 69, 70, 71, 72,
+   73, 74, 75,  76, 77, 78, 79, 80, 81, 82, 83, 84,
+   85, 86, 87,  88, 89, 90, 91, 92, 93, 94, 95, 96,
+   97, 98, 99, 100
+ ];
+ 
+let findNumber = 45;
+
+function search(array, value) {
+    let index = 0
+    let end = array.length - 1
+    let midpoint = Math.round(end / 2)
+    while (array[midpoint] !== value){
+        if (value > array[midpoint]) {
+            index = midpoint
+            midpoint = Math.round((end+index) / 2)
+        } else {
+            end = midpoint
+            midpoint = Math.round((end-index) / 2 ) + index - 1
+        }
+    }
+    return console.log(midpoint)
+}
+
+
+console.log(search(listNumber, findNumber))
